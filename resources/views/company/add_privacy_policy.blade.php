688abdfe0cf030082c837f9a6df7f345fdd4517b
Esther Eze <estherakowe@yahoo.com>
2024-09-04 17:03:29 +0100

    Update add_privacy_policy.blade.php

--- resources/views/company/add_privacy_policy.blade.php
+++ resources/views/company/add_privacy_policy.blade.php
@@ -1,63 +1,157 @@
 @extends('layouts.home')
-
-@extends('layouts.sidebar')
-
 @section('content')
 
-   <!-- ALL CART SECTION -->
-           <div class="adminx-content">
-        <!-- <div class="adminx-aside">
+<!-- Page header -->
+<div class="page-header d-print-none">
+      <div class="container-xl">
+            <div class="row g-2 align-items-center">
+                  <div class="col">
+                        <!-- Page pre-title -->
+                        <div class="page-pretitle">
+                              Privacy page
+                        </div>
+                        <h2 class="page-title">
+                              <span class=" d-none  d-md-block">Privacy Page</span>
 
-        </div> -->
+                        </h2>
+                  </div>
+                  <!-- Page title actions -->
+                  <div class="col-auto ms-auto d-print-none">
+                        <div class="btn-list">
 
-        <div class="adminx-main-content">
-          <div class="container-fluid">
-            <!-- container -->
-            <nav aria-label="breadcrumb" role="navigation">
-              <ol class="breadcrumb adminx-page-breadcrumb">
-                <li class="breadcrumb-item"><a href="{{ url('/') }}">Home</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Privacy Page</li>
-              </ol>
-            </nav>
-
-            <div class="pb-3">
-              <h4></h4>
-              <p class="text-danger"></p>
+                        </div>
+                  </div>
             </div>
-                <!-- row -->
-                <div class="row">
-                    <div class="col-lg-12 d-flex table-responsive">
-                    
-    	    
-            <table class="table">
-                <thead>
-                    <tr>
-                    <th></th>
-                    <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($about as $details)
-                    <tr>
-                        <td>{!! nl2br($details->privacy_policy) !!}</td>
-                        <td><a href="privacy_edit/{{$details->id}}"><i class="fa fa-edit"></i>Edit</a></td>
-                    </tr>
-                     
-            @endforeach
-                </tbody>
-                
-            </table>
-           
-        </div><!--col 12-->
-    </div><!--roww-->
+      </div>
+      <!-- Page body -->
+      <div class="page-body">
+            <!-- Alert start --->
+            <div class="container-xl">
+                  <div class="row ">
+                        <div class="col-12">
+                              <p></p>
+                              @if(session('approve'))
+                              <div class="alert  alert-success alert-dismissible" role="alert">
+                                    <div class="d-flex">
+                                          <div>
+                                                <!-- Download SVG icon from http://tabler-icons.io/i/alert-triangle -->
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon"
+                                                      width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                      stroke="currentColor" fill="none" stroke-linecap="round"
+                                                      stroke-linejoin="round">
+                                                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                      <path
+                                                            d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z" />
+                                                      <path d="M12 9v4" />
+                                                      <path d="M12 17h.01" />
+                                                </svg>
+                                          </div>
+                                          <div> {!! session('approve') !!}</div>
+                                    </div>
+                                    <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+                              </div>
+                              @endif
 
-</div>
-</div> <!-- section -->
+                              @if(session('remove'))
+                              <div class="alert  alert-success alert-dismissible" role="alert">
+                                    <div class="d-flex">
+                                          <div>
+                                                <!-- Download SVG icon from http://tabler-icons.io/i/alert-triangle -->
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon"
+                                                      width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                      stroke="currentColor" fill="none" stroke-linecap="round"
+                                                      stroke-linejoin="round">
+                                                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                      <path
+                                                            d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z" />
+                                                      <path d="M12 9v4" />
+                                                      <path d="M12 17h.01" />
+                                                </svg>
+                                          </div>
+                                          <div> {!! session('remove') !!}</div>
+                                    </div>
+                                    <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+                              </div>
+                              @endif
 
-<script type="text/javascript">
-    $(document).ready( function () {
-    $('#myTable').DataTable();
-} );
-</script>
 
-@endsection
+                              @if(session('success'))
+                              <div class="alert alert-important alert-success alert-dismissible" role="alert">
+                                    <div class="d-flex">
+                                          <div>
+                                                <!-- Download SVG icon from http://tabler-icons.io/i/alert-triangle -->
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon"
+                                                      width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                      stroke="currentColor" fill="none" stroke-linecap="round"
+                                                      stroke-linejoin="round">
+                                                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                      <path
+                                                            d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z" />
+                                                      <path d="M12 9v4" />
+                                                      <path d="M12 17h.01" />
+                                                </svg>
+                                          </div>
+                                          <div> {!! session('success') !!}</div>
+                                    </div>
+                                    <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+                              </div>
+                              @endif
+                        </div>
+                  </div>
+            </div>
+
+
+            <div class="container-xl">
+                  <div class="row row-deck row-cards">
+                        <div class="col-12">
+                              <div class="card">
+                                    <div class="card-header">
+                                          <h3 class="card-title"> </h3>
+                                    </div>
+
+                                    <div class="table-responsive " id="card">
+                                          <table class="table-striped table" id="table">
+
+                                                <thead>
+                                                      <tr>
+                                                            <th></th>
+                                                            <th></th>
+                                                      </tr>
+                                                </thead>
+                                                <tbody>
+                                                      @foreach($about as $details)
+                                                      <tr>
+                                                            <td>{!! nl2br($details->privacy_policy) !!}</td>
+                                                            <td><a href="privacy_edit/{{$details->id}}"><i
+                                                                              class="fa fa-edit"></i>Edit</a></td>
+                                                      </tr>
+
+                                                      @endforeach
+                                                </tbody>
+
+                                          </table>
+                                          </table>
+                                    </div>
+
+                                    <p></p>
+                                    <div class="card-footer d-flex align-items-center">
+                                    </div>
+
+                              </div>
+
+                        </div>
+
+
+                  </div>
+            </div>
+      </div>
+
+
+
+      <script type="text/javascript">
+      $(document).ready(function() {
+            $('#myTable').DataTable();
+      });
+      </script>
+
+      @endsection
