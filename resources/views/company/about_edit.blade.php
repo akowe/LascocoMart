6d4dafaa775edd80f255af9a13ffe64abe77a26e
Esther Eze <estherakowe@yahoo.com>
2024-09-04 16:57:32 +0100

    Update about_edit.blade.php

--- resources/views/company/about_edit.blade.php
+++ resources/views/company/about_edit.blade.php
@@ -9,7 +9,7 @@
                   <div class="col">
                         <!-- Page pre-title -->
                         <div class="page-pretitle">
-                        Edit About
+                              Edit About
                         </div>
                         <h2 class="page-title">
                               <span class=" d-none  d-md-block">About Page</span>
@@ -25,66 +25,136 @@
             </div>
       </div>
 
+      <!-- Page body -->
+      <div class="page-body">
+            <!-- Alert start --->
+            <div class="container-xl">
+                  <div class="row ">
+                        <div class="col-12">
+                              <p></p>
+                              @if(session('approve'))
+                              <div class="alert  alert-success alert-dismissible" role="alert">
+                                    <div class="d-flex">
+                                          <div>
+                                                <!-- Download SVG icon from http://tabler-icons.io/i/alert-triangle -->
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon"
+                                                      width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                      stroke="currentColor" fill="none" stroke-linecap="round"
+                                                      stroke-linejoin="round">
+                                                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                      <path
+                                                            d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z" />
+                                                      <path d="M12 9v4" />
+                                                      <path d="M12 17h.01" />
+                                                </svg>
+                                          </div>
+                                          <div> {!! session('approve') !!}</div>
+                                    </div>
+                                    <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+                              </div>
+                              @endif
 
-   <!-- ALL CART SECTION -->
-           <div class="adminx-content">
-        <!-- <div class="adminx-aside">
+                              @if(session('remove'))
+                              <div class="alert  alert-success alert-dismissible" role="alert">
+                                    <div class="d-flex">
+                                          <div>
+                                                <!-- Download SVG icon from http://tabler-icons.io/i/alert-triangle -->
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon"
+                                                      width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                      stroke="currentColor" fill="none" stroke-linecap="round"
+                                                      stroke-linejoin="round">
+                                                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                      <path
+                                                            d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z" />
+                                                      <path d="M12 9v4" />
+                                                      <path d="M12 17h.01" />
+                                                </svg>
+                                          </div>
+                                          <div> {!! session('remove') !!}</div>
+                                    </div>
+                                    <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+                              </div>
+                              @endif
 
-        </div> -->
 
-        <div class="adminx-main-content">
-          <div class="container-fluid">
-            <!-- container -->
-            <nav aria-label="breadcrumb" role="navigation">
-              <ol class="breadcrumb adminx-page-breadcrumb">
-                <li class="breadcrumb-item"><a href="{{url('/')}}">Home</a></li>
-                <li class="breadcrumb-item active" aria-current="page">About Page</li>
-              </ol>
-            </nav>
-
-            <div class="pb-3">
-              <h4>  @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                              @if(session('success'))
+                              <div class="alert alert-important alert-success alert-dismissible" role="alert">
+                                    <div class="d-flex">
+                                          <div>
+                                                <!-- Download SVG icon from http://tabler-icons.io/i/alert-triangle -->
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon"
+                                                      width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                      stroke="currentColor" fill="none" stroke-linecap="round"
+                                                      stroke-linejoin="round">
+                                                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                      <path
+                                                            d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z" />
+                                                      <path d="M12 9v4" />
+                                                      <path d="M12 17h.01" />
+                                                </svg>
+                                          </div>
+                                          <div> {!! session('success') !!}</div>
+                                    </div>
+                                    <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+                              </div>
+                              @endif
                         </div>
-                    @endif</h4>
-              <p class="text-danger"></p>
+                  </div>
             </div>
-                <!-- row -->
-                <div class="row">
-                    <div class="col-lg-12 d-flex table-responsive">
-                    
-    	       <form action="{{ url('about_update/'.$about->id) }}" method="POST">
-                @csrf
-                @method('PUT')
-                    <div class="form-group">
-                     <h6>About</h6>
-                        <textarea type="text" name="about" rows="5" cols="120" style="text-align:justify !important; display: flex; padding:10px;">
+
+
+            <div class="container-xl">
+                  <div class="row row-deck row-cards">
+                        <div class="col-12">
+                              <form action="{{ url('about_update/'.$about->id) }}" method="POST">
+                                    @csrf
+                                    @method('PUT')
+                                    <div class="card">
+                                          <div class="card-header">
+                                                <h3 class="card-title"> </h3>
+                                          </div>
+                                          <div class="card-body">
+                                                <div class="row">
+                                                      <div class="col-md-12">
+
+                                                            <div class="form-group">
+                                                                  <h6>About</h6>
+                                                                  <textarea type="text" name="about" rows="5" cols="120"
+                                                                        style="text-align:justify !important; display: flex; padding:10px;">
                             {{$about->about}}
                             </textarea>
-                    </div>
-                    <div class="form-group">
-                        <h6>Our Story</h6>
-                        <textarea type="text" name="our_story"  rows="15" cols="120" style="text-align:justify !important; display: flex; padding:10px;">
+                                                            </div>
+                                                            <div class="form-group">
+                                                                  <h6>Our Story</h6>
+                                                                  <textarea type="text" name="our_story" rows="15"
+                                                                        cols="120"
+                                                                        style="text-align:justify !important; display: flex; padding:10px;">
                             {{$about->our_story}}
                             </textarea>
-                     </div>
-                          
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-outline-danger">Update</button>
-                    </div>
-                </form>
-           
-        </div><!--col 12-->
-    </div><!--roww-->
+                                                            </div>
 
-</div>
-</div> <!-- section -->
+                                                            <div class="form-group">
+                                                                  <button type="submit"
+                                                                        class="btn btn-outline-danger">Update</button>
+                                                            </div>
+                                                      </div>
+                                                      <!--col 12-->
+                                                </div>
+                                          </div>
 
-<script type="text/javascript">
-    $(document).ready( function () {
-    $('#myTable').DataTable();
-} );
-</script>
+                                    </div> <!-- CARD SECTION -->
+                              </form>
 
-@endsection
+                        </div>
+                  </div>
+            </div>
+      </div>
+
+   
+            <script type="text/javascript">
+            $(document).ready(function() {
+                  $('#myTable').DataTable();
+            });
+            </script>
+
+            @endsection
