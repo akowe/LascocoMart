b9fa34df7ed49fb90518d18310a71ee4d18a1f36
Esther Eze <estherakowe@yahoo.com>
2024-09-04 17:09:22 +0100

    Update refund_edit.blade.php

--- resources/views/company/refund_edit.blade.php
+++ resources/views/company/refund_edit.blade.php
@@ -1,61 +1,149 @@
 @extends('layouts.home')
-
-@extends('layouts.sidebar')
-
 @section('content')
-
-   <!-- ALL CART SECTION -->
-           <div class="adminx-content">
-        <!-- <div class="adminx-aside">
-
-        </div> -->
-
-        <div class="adminx-main-content">
-          <div class="container-fluid">
-            <!-- container -->
-            <nav aria-label="breadcrumb" role="navigation">
-              <ol class="breadcrumb adminx-page-breadcrumb">
-                <li class="breadcrumb-item"><a href="{{url('/')}}">Home</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Return & Refund Page</li>
-              </ol>
-            </nav>
-
-            <div class="pb-3">
-              <h4>  @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+<!-- Page header -->
+<div class="page-header d-print-none">
+      <div class="container-xl">
+            <div class="row g-2 align-items-center">
+                  <div class="col">
+                        <!-- Page pre-title -->
+                        <div class="page-pretitle">
+                              Edit Return & Refund
                         </div>
-                    @endif</h4>
-              <p class="text-danger"></p>
+                        <h2 class="page-title">
+                              <span class=" d-none  d-md-block">Return & Refund Page</span>
+
+                        </h2>
+                  </div>
+                  <!-- Page title actions -->
+                  <div class="col-auto ms-auto d-print-none">
+                        <div class="btn-list">
+
+                        </div>
+                  </div>
             </div>
-                <!-- row -->
-                <div class="row">
-                    <div class="col-lg-12 d-flex ">
-                    
-    	       <form action="{{ url('refund_update/'.$about->id) }}" method="POST">
-                @csrf
-                @method('PUT')
-                    <div class="form-group">
-                     <h6>Return Policy</h6>
-                        <textarea type="text" name="return" rows="75" cols="120" style="text-align:justify !important; display: flex; padding: 10px;">
+      </div>
+
+      <!-- Page body -->
+      <div class="page-body">
+            <!-- Alert start --->
+            <div class="container-xl">
+                  <div class="row ">
+                        <div class="col-12">
+                              <p></p>
+                              @if(session('status'))
+                              <div class="alert  alert-success alert-dismissible" role="alert">
+                                    <div class="d-flex">
+                                          <div>
+                                                <!-- Download SVG icon from http://tabler-icons.io/i/alert-triangle -->
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon"
+                                                      width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                      stroke="currentColor" fill="none" stroke-linecap="round"
+                                                      stroke-linejoin="round">
+                                                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                      <path
+                                                            d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z" />
+                                                      <path d="M12 9v4" />
+                                                      <path d="M12 17h.01" />
+                                                </svg>
+                                          </div>
+                                          <div> {!! session('status') !!}</div>
+                                    </div>
+                                    <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+                              </div>
+                              @endif
+
+                              @if(session('remove'))
+                              <div class="alert  alert-success alert-dismissible" role="alert">
+                                    <div class="d-flex">
+                                          <div>
+                                                <!-- Download SVG icon from http://tabler-icons.io/i/alert-triangle -->
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon"
+                                                      width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                      stroke="currentColor" fill="none" stroke-linecap="round"
+                                                      stroke-linejoin="round">
+                                                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                      <path
+                                                            d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z" />
+                                                      <path d="M12 9v4" />
+                                                      <path d="M12 17h.01" />
+                                                </svg>
+                                          </div>
+                                          <div> {!! session('remove') !!}</div>
+                                    </div>
+                                    <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+                              </div>
+                              @endif
+
+
+                              @if(session('success'))
+                              <div class="alert alert-important alert-success alert-dismissible" role="alert">
+                                    <div class="d-flex">
+                                          <div>
+                                                <!-- Download SVG icon from http://tabler-icons.io/i/alert-triangle -->
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon"
+                                                      width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                      stroke="currentColor" fill="none" stroke-linecap="round"
+                                                      stroke-linejoin="round">
+                                                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                      <path
+                                                            d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z" />
+                                                      <path d="M12 9v4" />
+                                                      <path d="M12 17h.01" />
+                                                </svg>
+                                          </div>
+                                          <div> {!! session('success') !!}</div>
+                                    </div>
+                                    <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+                              </div>
+                              @endif
+                        </div>
+                  </div>
+            </div>
+
+
+
+            <div class="container-xl">
+                  <div class="row row-deck row-cards">
+                        <div class="col-12">
+                        <form action="{{ url('refund_update/'.$about->id) }}" method="POST">
+                                          @csrf
+                                          @method('PUT')
+                                    <div class="card">
+                                          <div class="card-header">
+                                                <h3 class="card-title"> </h3>
+                                          </div>
+                                          <div class="card-body">
+                                                <div class="row">
+                                                      <div class="col-md-12">
+                                                            <div class="form-group">
+                                                                  <h6>Return Policy</h6>
+                                                                  <textarea type="text" name="return" rows="75"
+                                                                        cols="120"
+                                                                        style="text-align:justify !important; display: flex; padding: 10px;">
                             {{$about->return_policy}}
                             </textarea>
-                    </div>
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-outline-danger">Update</button>
-                    </div>
-                </form>
-           
-        </div><!--col 12-->
-    </div><!--roww-->
+                                                            </div>
+                                                            <div class="form-group">
+                                                                  <button type="submit"
+                                                                        class="btn btn-outline-danger">Update</button>
+                                                            </div>
+                                                      </div>
+                                                      <!--col 12-->
+                                                </div>
+                                          </div>
 
-</div>
-</div> <!-- section -->
+                                    </div> <!-- CARD SECTION -->
+                              </form>
 
-<script type="text/javascript">
-    $(document).ready( function () {
-    $('#myTable').DataTable();
-} );
-</script>
+                        </div>
+                  </div>
+            </div>
+      </div>
 
-@endsection
+            <script type="text/javascript">
+            $(document).ready(function() {
+                  $('#myTable').DataTable();
+            });
+            </script>
+
+            @endsection
