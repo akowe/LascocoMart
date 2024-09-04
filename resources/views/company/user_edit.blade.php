e02d99a94586c4ad403a3ea9a65974bbda1c42e4
Esther Eze <estherakowe@yahoo.com>
2024-09-04 11:23:52 +0100

    update edit user

--- resources/views/company/user_edit.blade.php
+++ resources/views/company/user_edit.blade.php
@@ -1,125 +1,184 @@
 @extends('layouts.home')
-
-@extends('layouts.sidebar')
-
 @section('content')
+<!-- Page header -->
 
-<!-- ALL CART SECTION -->
-<div class="adminx-content">
+<div class="page-header d-print-none">
+      <div class="container-xl">
+            <div class="row g-2 align-items-center">
+                  <div class="col">
+                        <!-- Page pre-title -->
+                        <div class="page-pretitle">
+                              Users
+                        </div>
+                        <h2 class="page-title">
+                              <span class=" d-none  d-md-block">Edit</span>
 
-      <div class="adminx-main-content">
-            <div class="container-fluid">
-                  <!-- container -->
-                  <nav aria-label="breadcrumb" role="navigation">
-                        <ol class="breadcrumb adminx-page-breadcrumb">
-                              <li class="breadcrumb-item"><a href="{{url('/')}}">Home</a></li>
-                              <li class="breadcrumb-item active" aria-current="page">Edit User</li>
-                        </ol>
-                  </nav>
-
-                  <div class="pb-3">
-                        <h4> @if (session('status'))
-                              <div class="alert alert-success" role="alert">
-                                    {{ session('status') }}
-                              </div>
-                              @endif
-                        </h4>
-                        <p class="text-danger"></p>
+                        </h2>
                   </div>
-                  <!-- row -->
-                  <form action="{{ url('user_update/'.$users->id) }}" method="POST">
-
-                        <div class="row">
-                              <div class="col-md-5 ">
-                                    @csrf
-                                    @method('PUT')
-
-                                    <div class="form-group">
-                                          <h6>User ID</h6>
-                                          <input type="text" value="{{$users->code}}" name="code" class="form-control"
-                                                readonly>
-                                    </div>
-                                    <div class="form-group">
-                                          <h6> Email</h6>
-                                          <input type="text" value="{{$users->email}}" name="email"
-                                                class="form-control">
-                                    </div>
-                                    <div class="form-group">
-                                          <h6> Phone</h6>
-                                          <input type="text" value="{{$users->phone}}" name="phone"
-                                                class="form-control">
-                                    </div>
-                                    <div class="form-group">
-                                          <h6>Cooperative</h6>
-                                          <input type="text" value="{{$users->coopname}}" name="coopname"
-                                                class="form-control">
-                                    </div>
-
-                                    <div class="form-group">
-                                          <h6>First Name</h6>
-                                          <input type="text" value="{{$users->fname}}" name="fname"
-                                                class="form-control">
-                                    </div>
-
-                                    <div class="form-group">
-                                          <h6>Last Name</h6>
-                                          <input type="text" value="{{$users->lname}}" name="lname"
-                                                class="form-control">
-                                    </div>
-
-                              </div>
-
-                              <div class="col-lg-5">
-                                    <div class="form-group">
-                                          <h6> Address</h6>
-                                          <textarea type="text" value="{{$users->address}}" name="address" row="3"
-                                                col="3" class="form-control">{{$users->address}}</textarea>
-                                    </div>
-                                    <div class="form-group">
-                                          <h6>Location</h6>
-                                          <input type="text" value="{{$users->location}}" name="location"
-                                                class="form-control">
-                                    </div>
-                                    <div class="form-group">
-                                          <h6>Bank Name</h6>
-                                          <input type="text" value="{{$users->bank}}" name="bank" class="form-control">
-                                    </div>
-
-                                    <div class="form-group">
-                                          <h6>Account Name</h6>
-                                          <input type="text" value="{{$users->account_name}}" name="account_name"
-                                                class="form-control">
-                                    </div>
-
-                                    <div class="form-group">
-                                          <h6>Account Number</h6>
-                                          <input type="text" value="{{$users->account_number}}" name="account_number"
-                                                class="form-control">
-                                    </div>
-                                    <div class="form-group">
-                                          <button type="submit" class="btn btn-outline-danger btn-block"><i
-                                                      class="fa fa-arrow-up"></i> Save Changes</button>
-                                    </div>
-
-                              </div>
-                  </form>
-                  <div class="col-lg-2">
+                  <!-- Page title actions -->
+                  <div class="col-auto ms-auto d-print-none">
+                        <div class="btn-list">
                         <div class="form-group">
                               <input type="hidden" name="" id="user_id" value="{{$users->id}}">
-                              <a onclick="resetPassword();" class="btn btn-primary btn-block text-white"><i class="fa fa-refresh"></i> Reset Password
+                              <a onclick="resetPassword();" class="btn btn-primary btn-block text-white"><i
+                                          class="fa fa-refresh"></i>  &nbsp; Reset Password
                               </a>
-                            
+
                         </div>
-
+                        </div>
                   </div>
-                  <!--col 4--->
-
             </div>
-            <!--roww-->
-
-
       </div>
-</div> <!-- section -->
+</div>
+
+
+
+<!-- Page body -->
+<div class="page-body">
+      <!-- Alert start --->
+      <div class="container-xl">
+            <div class="row ">
+                  <div class="col-12">
+                        <p></p>
+                        @if (session('status'))
+                        <div class="alert  alert-success alert-dismissible" role="alert">
+                              <div class="d-flex">
+                                    <div>
+                                          <!-- Download SVG icon from http://tabler-icons.io/i/check -->
+                                          <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon" width="24"
+                                                height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                                fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                <path d="M5 12l5 5l10 -10" />
+                                          </svg>
+
+                                    </div>
+                                    <div>{{ session('status') }}</div>
+                              </div>
+                              <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+                        </div>
+                        @endif
+
+                        @if (session('users-status'))
+                        <div class="alert  alert-danger alert-dismissible" role="alert">
+                              <div class="d-flex">
+                                    <div>
+                                          <!-- Download SVG icon from http://tabler-icons.io/i/check -->
+                                          <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon" width="24"
+                                                height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                                fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                <path d="M5 12l5 5l10 -10" />
+                                          </svg>
+
+                                    </div>
+                                    <div>{{ session('users-status') }}</div>
+                              </div>
+                              <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+                        </div>
+                        @else
+                        @endif
+                  </div>
+            </div>
+      </div>
+      <!-- Alert stop --->
+
+
+      <div class="page-header d-print-none">
+            <div class="container-xl">
+                  <div class="row row-cards">
+                        <div class="col-12">
+                              <form action="{{ url('user_update/'.$users->id) }}" method="POST">
+
+                                    @csrf
+                                    <div class="card">
+                                          <div class="card-body p-4">
+                                                <div class="row">
+                                                      <div class="col-md-6 ">
+                                                            @method('PUT')
+
+                                                            <div class="form-group">
+                                                                  <h6>User ID</h6>
+                                                                  <input type="text" value="{{$users->code}}"
+                                                                        name="code" class="form-control" readonly>
+                                                            </div>
+                                                            <br>
+                                                            <div class="form-group">
+                                                                  <h6> Email</h6>
+                                                                  <input type="text" value="{{$users->email}}"
+                                                                        name="email" class="form-control">
+                                                            </div>
+                                                            <br>
+                                                            <div class="form-group">
+                                                                  <h6> Phone</h6>
+                                                                  <input type="text" value="{{$users->phone}}"
+                                                                        name="phone" class="form-control">
+                                                            </div>
+                                                            <br>
+                                                            <div class="form-group">
+                                                                  <h6>Cooperative</h6>
+                                                                  <input type="text" value="{{$users->coopname}}"
+                                                                        name="coopname" class="form-control">
+                                                            </div>
+                                                            <br>
+                                                            <div class="form-group">
+                                                                  <h6>First Name</h6>
+                                                                  <input type="text" value="{{$users->fname}}"
+                                                                        name="fname" class="form-control">
+                                                            </div>
+                                                            <br>
+
+                                                      </div>
+
+                                                      <div class="col-lg-6">
+                                                            <div class="form-group">
+                                                                  <h6> Address</h6>
+                                                                  <textarea type="text" value="{{$users->address}}"
+                                                                        name="address" row="1" col="3"
+                                                                        class="form-control">{{$users->address}}</textarea>
+                                                            </div>
+                                                            <br>
+                                                            <div class="form-group">
+                                                                  <h6>Location</h6>
+                                                                  <input type="text" value="{{$users->location}}"
+                                                                        name="location" class="form-control">
+                                                            </div>
+                                                            <br>
+                                                            <div class="form-group">
+                                                                  <h6>Bank Name</h6>
+                                                                  <input type="text" value="{{$users->bank}}"
+                                                                        name="bank" class="form-control">
+                                                            </div>
+                                                            <br>
+                                                            <div class="form-group">
+                                                                  <h6>Account Name</h6>
+                                                                  <input type="text" value="{{$users->account_name}}"
+                                                                        name="account_name" class="form-control">
+                                                            </div>
+                                                            <br>
+                                                            <div class="form-group">
+                                                                  <h6>Account Number</h6>
+                                                                  <input type="text" value="{{$users->account_number}}"
+                                                                        name="account_number" class="form-control">
+                                                            </div>
+                                                          
+                                                      </div>
+
+                                                      <div class="form-group">
+                                                                  <button type="submit"
+                                                                        class="btn btn-outline-danger btn-block"><i
+                                                                              class="fa fa-arrow-up"></i> Save
+                                                                        Changes</button>
+                                                            </div>
+                                                </div>
+                                          </div>
+                                    </div>
+                                    <!-- card end -->
+                              </form>
+                        </div>
+                  </div>
+            </div>
+      </div>
 </div>
 
 <script type="text/javascript">
@@ -137,14 +196,13 @@ function resetPassword() {
             var id = document.getElementById('user_id').value;
             var showRoute = "{{ route('reset-user-password', ':id') }}";
             url = showRoute.replace(':id', id);
-            
+
             window.location = url;
 
       } else {
             // window.location.reload();
       }
 }
-
 </script>
 
 @endsection
