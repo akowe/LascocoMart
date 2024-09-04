de5eadf99b8887fd91b4ec91f5cc197930eefc66
Esther Eze <estherakowe@yahoo.com>
2024-09-04 16:51:54 +0100

    Update about_edit.blade.php

--- resources/views/company/about_edit.blade.php
+++ resources/views/company/about_edit.blade.php
@@ -1,6 +1,31 @@
 @extends('layouts.home')
 @section('content')
 
+
+<!-- Page header -->
+<div class="page-header d-print-none">
+      <div class="container-xl">
+            <div class="row g-2 align-items-center">
+                  <div class="col">
+                        <!-- Page pre-title -->
+                        <div class="page-pretitle">
+                        Edit About
+                        </div>
+                        <h2 class="page-title">
+                              <span class=" d-none  d-md-block">About Page</span>
+
+                        </h2>
+                  </div>
+                  <!-- Page title actions -->
+                  <div class="col-auto ms-auto d-print-none">
+                        <div class="btn-list">
+
+                        </div>
+                  </div>
+            </div>
+      </div>
+
+
    <!-- ALL CART SECTION -->
            <div class="adminx-content">
         <!-- <div class="adminx-aside">
