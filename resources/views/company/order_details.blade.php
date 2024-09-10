ea97e508b4b1362662577bc06ed39fc529316868
Esther Eze <estherakowe@yahoo.com>
2024-09-10 15:38:37 +0100

    update order details

--- resources/views/company/order_details.blade.php
+++ resources/views/company/order_details.blade.php
@@ -164,47 +164,33 @@
                                                       <td><input class="form-check-input m-0 align-middle"
                                                                   type="checkbox" aria-label="Select"></td>
                                                       <td><span
-                                                                  class="text-secondary">{{ date('d/M/Y', strtotime($details->date))}}</span>
+                                                                  class="text-secondary">{{ date('d/M/Y', strtotime($order->date))}}</span>
                                                       </td>
 
                                                     
-                                                <td>{{ $details['coopname'] }}<br>
-                                                      {{ $details['fname'] }}
+                                                <td>{{ $order['coopname'] }}<br>
+                                                      {{ $order['fname'] }}
                                                       <br>
-                                                      {{ $details['lname'] }}
+                                                      {{ $order['lname'] }}
 
                                                       <br>
-                                                      {{ $details['email'] }}
+                                                      {{ $order['email'] }}
                                                       <br>
-                                                      {{ $details['phone'] }}
+                                                      {{ $order['phone'] }}
                                                       <br>
-                                                      {{ $details['address'] }}
+                                                      {{ $order['address'] }}
                                                       <br>
-                                                      {{ $details['location'] }}
+                                                      {{ $order['location'] }}
                                                 </td>
 
-                                                <td> <a href="{{ route ('sales_invoice', $details->order_number) }}"
-                                                            title="Click to view">{{$details['order_number'] }}</a></td>
-                                                <td> {{ $details['prod_name'] }}</td>
-                                                <td> {{ number_format($details['price']) }}</td>
-                                                <td> {{ number_format($details['seller_price']) }}</td>
-                                                <td>{{ $details['pay_type'] }} </td>
+                                                <td> <a href="{{ route ('sales_invoice', $order->order_number) }}"
+                                                            title="Click to view">{{$order['order_number'] }}</a></td>
+                                                <td> {{ $order['prod_name'] }}</td>
+                                                <td> {{ number_format($order['price']) }}</td>
+                                                <td> {{ number_format($order['seller_price']) }}</td>
+                                                <td>{{ $order['pay_type'] }} </td>
 
-                                                      <td>{{ number_format($order->grandtotal) }}</td>
-                                                      <td> {{$order['order_number'] }}</td>
-                                                      <td>
-                                                            @if($order->status =='paid')
-                                                            <span class="badge bg-green-lt">{{$order->status}}</span>
-
-                                                            @elseif($order->status =='pending')
-                                                            <span class="badge bg-yellow-lt">{{$order->status}}</span>
-
-                                                            @elseif($order->status =='cancel')
-                                                            <span class="badge bg-red-lt">{{$order->status}}</span>
-                                                            @else
-
-                                                            @endif
-                                                      </td>
+                     
 
 
                                                 </tr>
@@ -272,98 +258,7 @@
 </div>
 <!-- Page body -->
 
-<!-- ALL CART SECTION -->
-<div class="adminx-content">
-      <!-- <div class="adminx-aside">
 
-        </div> -->
-
-      <div class="adminx-main-content">
-            <div class="container-fluid">
-                  <!-- container -->
-                  <nav aria-label="breadcrumb" role="navigation">
-                        <ol class="breadcrumb adminx-page-breadcrumb">
-                              <li class="breadcrumb-item"><a href="{{ url('/') }}">Home</a></li>
-                              <li class="breadcrumb-item active" aria-current="page">Order Details</li>
-                        </ol>
-                  </nav>
-
-                  <div class="pb-3">
-                        <h4>Details</h4>
-                        <p class="text-danger"></p>
-                  </div>
-
-                  <!-- row -->
-                  <div class="row">
-                        <div class="col-lg-12 d-flex table-responsive">
-                              <table class="table table-striped " id="table">
-                                    <thead>
-                                          <tr>
-
-                                                <th>Payment Date</th>
-                                                <th>Seller Details</th>
-                                                <th>Order Number</th>
-                                                <th>Product</th>
-                                                <th>Company Price</th>
-                                                <th>Seller Price</th>
-                                                <th>Payment Type</th>
-                                          </tr>
-                                    </thead>
-                                    <tbody>
-
-                                          @foreach($orders as $details)
-                                          <tr>
-
-                                                <td>
-                                                      {{ date('d/M/Y', strtotime($details->date))}}</td>
-
-
-                                                <td>{{ $details['coopname'] }}<br>
-                                                      {{ $details['fname'] }}
-                                                      <br>
-                                                      {{ $details['lname'] }}
-
-                                                      <br>
-                                                      {{ $details['email'] }}
-                                                      <br>
-                                                      {{ $details['phone'] }}
-                                                      <br>
-                                                      {{ $details['address'] }}
-                                                      <br>
-                                                      {{ $details['location'] }}
-                                                </td>
-                                                <td> <a href="{{ route ('sales_invoice', $details->order_number) }}"
-                                                            title="Click to view">{{$details['order_number'] }}</a></td>
-                                                <td> {{ $details['prod_name'] }}</td>
-                                                <td> {{ number_format($details['price']) }}</td>
-                                                <td> {{ number_format($details['seller_price']) }}</td>
-                                                <td>
-                                                      {{ $details['pay_type'] }}
-
-                                                </td>
-
-                                                </form>
-
-
-                                                </td>
-                                          </tr>
-
-                                          @endforeach
-
-
-
-                                    </tbody>
-                              </table>
-
-                        </div>
-                        <!--col 12-->
-                        {{$orders->links()}}
-                  </div>
-                  <!--roww-->
-
-            </div>
-
-      </div> <!-- section -->
 
       <script type="text/javascript">
       $(document).ready(function() {
