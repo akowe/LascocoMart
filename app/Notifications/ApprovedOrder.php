403edd400d623e11f044a82e6db3a0c28d70caa0
Esther Eze <estherakowe@yahoo.com>
2024-09-11 17:02:08 +0100

    update notification approvedOrder

--- app/Notifications/ApprovedOrder.php
+++ app/Notifications/ApprovedOrder.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
 use App\Models\User;
 use Auth;
 
