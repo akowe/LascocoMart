5475503f6705e358460f57b7cb2f0adf67c9affb
Esther Eze <estherakowe@yahoo.com>
2024-09-12 06:17:42 +0100

    Update history.blade.php

--- resources/views/wallet/history.blade.php
+++ resources/views/wallet/history.blade.php
@@ -8,14 +8,14 @@
                   <div class="col">
                         <!-- Page pre-title -->
                         <div class="page-pretitle">
-                              History
+                             History
                         </div>
                         <h2 class="page-title">
                               <span class=" d-none  d-md-block">Wallet</span>
                         </h2>
                   </div>
                   <!-- Page title actions -->
-
+          
                   <div class="col-auto ms-auto d-print-none">
                         <div class="btn-list">
                               <span class="d-block ">
@@ -34,10 +34,10 @@
                                                 d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
                                     </svg>
 
-                                    View Wallet
+                                  View Wallet
                               </a>
                               <a href="{{ url('wallet')  }}" class="btn btn-danger d-sm-none btn-icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye"
+                              <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye"
                                           width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5"
                                           stroke="currentColor" fill="none" stroke-linecap="round"
                                           stroke-linejoin="round">
@@ -50,7 +50,7 @@
                               </a>
                         </div>
                   </div>
-
+                
 
             </div>
       </div>
@@ -64,141 +64,54 @@
                               <div class="card-header">
                                     <h3 class="card-title">Wallet History </h3>
                               </div>
-                              <div class="card-body border-bottom py-3">
-                                    <div class="d-flex">
-                                          <div class="text-secondary">
-                                                Show
-                                                <div class="mx-2 d-inline-block">
-                                                      <select id="pagination" class="form-control form-control-sm"
-                                                            name="perPage">
-                                                            <option value="5" @if($perPage==5) selected @endif>5
-                                                            </option>
-                                                            <option value="10" @if($perPage==10) selected @endif>10
-                                                            </option>
-                                                            <option value="25" @if($perPage==25) selected @endif>25
-                                                            </option>
-                                                            <option value="50" @if($perPage==50) selected @endif>50
-                                                            </option>
-                                                      </select>
-                                                </div>
-                                                records
-                                          </div>
-                                          <div class="ms-auto text-secondary">
-                                                Search:
-                                                <div class="ms-2 d-inline-block">
+                              <table class="table card-table table-vcenter text-nowrap datatable" id="orders">
+                                    <thead>
+                                          <tr>
+                                                <th class="w-1"><input class="form-check-input m-0 align-middle"
+                                                            type="checkbox" aria-label="Select all product">
+                                                </th>
 
-                                                      <form action="/wallet-history" method="GET" role="search">
-                                                            {{ csrf_field() }}
-                                                            <div class="input-group mb-2">
-                                                                  <input type="text" class="form-control"
-                                                                        placeholder="Search for…" name="search">
-                                                                  <button type="submit" class="btn"
-                                                                        type="button">Go!</button>
-                                                            </div>
-                                                      </form>
-                                                </div>
-                                          </div>
-                                    </div>
-                              </div>
+                                                <th>Transaction Ref.</th>
+                                                <th>Amount</th>
+                                                <th>Description </th>
+                                                <th>Balance</th>
+                                                <th>Date</th>
 
-                              <div class="table-responsive" id="card">
-                                    <table class="table card-table table-vcenter text-nowrap datatable" id="orders">
-                                          <thead>
-                                                <tr>
-                                                      <th class="w-1"><input class="form-check-input m-0 align-middle"
-                                                                  type="checkbox" aria-label="Select all product">
-                                                      </th>
+                                          </tr>
+                                    </thead>
 
-                                                      <th>Transaction Ref.</th>
-                                                      <th>Amount</th>
-                                                      <th>Description </th>
-                                                      <th>Balance</th>
-                                                      <th>Date</th>
+                                    <tbody>
 
-                                                </tr>
-                                          </thead>
-
-                                          <tbody>
-
-                                                @if(empty($data))
-                                                @else
-                                                @foreach($data as $data)
-                                                <tr>
-                                                      <td><input class="form-check-input m-0 align-middle"
-                                                                  type="checkbox" aria-label="Select"></td>
-                                                      <td>{{$data['reference']}}</td>
+                                          @if(empty($data))
+                                          @else
+                                          @foreach($data as $data)
+                                          <tr>
+                                                <td><input class="form-check-input m-0 align-middle" type="checkbox"
+                                                            aria-label="Select"></td>
+                                                <td>{{$data['reference']}}</td>
 
 
-                                                      <td>
-                                                            @if(Str::contains($data['narration'], 'CREDIT'))
-                                                            {{$data['amount']}} <small> <span
-                                                                        class="badge bg-green-lt">Credit</span></small>
-                                                            @else
-                                                            {{$data['amount']}} <small><span
-                                                                        class="badge bg-danger-lt">Debit</span></small>
-                                                            @endif
-                                                      </td>
+                                                <td>
+                                                      @if(Str::contains($data['narration'], 'CREDIT'))
+                                                      {{$data['amount']}} <small> <span
+                                                                  class="badge bg-green-lt">Credit</span></small>
+                                                      @else
+                                                      {{$data['amount']}} <small><span
+                                                                  class="badge bg-danger-lt">Debit</span></small>
+                                                      @endif
+                                                </td>
 
-                                                      <td>{{$data['narration']}}</td>
-                                                      <td>{{$data['balance']}}</td>
-                                                      <td>{{ date('m/d/Y', strtotime($data['transaction_date']))}}
-                                                      </td>
+                                                <td>{{$data['narration']}}</td>
+                                                <td>{{$data['balance']}}</td>
+                                                <td>{{ date('m/d/Y', strtotime($data['transaction_date']))}}
+                                                </td>
 
-                                                </tr>
-                                                @endforeach
+                                          </tr>
+                                          @endforeach
 
-                                                @endif
-                                          </tbody>
-                                    </table>
-                              </div>
-                              <div class="card-footer d-flex align-items-center">
-                                    <p class="m-0 text-secondary">
-
-                                          Showing {{ ($data->currentPage() - 1) * $data->perPage() + 1; }} to
-                                          {{ min($data->currentPage()* $data->perPage(), $data->total()) }} of
-                                          {{$data->total()}} entries
-                                    </p>
-
-                                    <ul class="pagination m-0 ms-auto">
-                                          @if(isset($data))
-                                          @if($data->currentPage() > 1)
-                                          <li class="page-item ">
-                                                <a class="page-link text-danger" href="{{ $data->previousPageUrl() }}"
-                                                      tabindex="-1" aria-disabled="true">
-                                                      <!-- Download SVG icon from http://tabler-icons.io/i/chevron-left -->
-                                                      <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24"
-                                                            height="24" viewBox="0 0 24 24" stroke-width="2"
-                                                            stroke="currentColor" fill="none" stroke-linecap="round"
-                                                            stroke-linejoin="round">
-                                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                            <path d="M15 6l-6 6l6 6" />
-                                                      </svg>
-                                                      prev
-                                                </a>
-                                          </li>
                                           @endif
-
-
-                                          <li class="page-item"> {{ $data->appends(compact('perPage'))->links()  }}
-                                          </li>
-                                          @if($data->hasMorePages())
-                                          <li class="page-item">
-                                                <a class="page-link text-danger" href="{{ $data->nextPageUrl() }}">
-                                                      next
-                                                      <!-- Download SVG icon from http://tabler-icons.io/i/chevron-right -->
-                                                      <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24"
-                                                            height="24" viewBox="0 0 24 24" stroke-width="2"
-                                                            stroke="currentColor" fill="none" stroke-linecap="round"
-                                                            stroke-linejoin="round">
-                                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                            <path d="M9 6l6 6l-6 6" />
-                                                      </svg>
-                                                </a>
-                                          </li>
-                                          @endif
-                                          @endif
-                                    </ul>
-                              </div>
+                                    </tbody>
+                              </table>
                         </div>
                   </div>
             </div>
