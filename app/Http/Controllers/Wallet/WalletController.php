363d1659f1ff427fc1111b51af037162f4d0c351
Esther Eze <estherakowe@yahoo.com>
2024-09-15 20:21:30 +0100

    Update WalletController.php

--- app/Http/Controllers/Wallet/WalletController.php
+++ app/Http/Controllers/Wallet/WalletController.php
@@ -90,14 +90,7 @@ class WalletController extends Controller
             $lastTenDays = Carbon::now()->subDays(10)->format('Y-m-d');
             $todayDate = Carbon::now()->format('Y-m-d');
             //Ogaranya Wallet Account 
-          //staging: https://api.staging.ogaranya.com/v1/2347033141516/wallet
-          //'token: e4f3f028-c0b4-4c9b-b8ef-8be41a7613f6',
-          //'publickey: 62f2da03d13992642d5416b3b1977071bf3adfe99a93b8daea6194306b168b84901f49025f25a245f083b0d627c921f5642ff124047e4a143dfe4cc1dd526d1b',
-          
-          //production:  https://api.ogaranya.com/v1/2347033141516/wallet
-          // 'token: MDY0OTgzMTkxNjIzNGViZDA3YWIxZWMwZTFjYzY2Mzk1OTAwYjYwNTc2ZjY4NzBlOTBlMGQzMjk5YzJlZmUxZA==',
-          // 'publickey: 4f223ac9cff724d03833fb8fb9e1a0638dc5125696420cc33c71bcf2e35a0af08beb8cd85a0c0c2eca2670d0244ca70bb9dff6bfa081def75cdaab1034beb1fe',
-          $data = array(
+            $data = array(
             "phone"            => $phoneNumber,
             "account_number"   => $WalletAccountNumber,
             );
