d7bb309ab7a34780fbbef74286d68a629a2623b3
Esther Eze <estherakowe@yahoo.com>
2024-09-23 18:33:59 +0100

    Update member-order.blade.php

--- resources/views/cooperative/member-order.blade.php
+++ resources/views/cooperative/member-order.blade.php
@@ -601,7 +601,7 @@
       <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                   <div class="modal-header">
-                        <h5 class="modal-title">Add Fund To Wallet </h5>
+                  <h5 class="modal-title">In-partnership with <span class="text-danger">OGARANYA PAY</span>. Add Fund To Wallet </h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <div class="modal-body">
