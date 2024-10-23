521b06c1f16a3672c0e7be181992531ecf09bc4a
Esther Eze <estherakowe@yahoo.com>
2024-10-23 14:06:06 +0100

    update product

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -44,15 +44,24 @@
                         <ol class="carousel-indicators">
                               <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
                               <li data-target="#myCarousel" data-slide-to="1"></li>
-                              <li data-target="#myCarousel" data-slide-to="2"></li>
                         </ol>
+
+                          <!-- Left and right controls -->
+                          <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                              <span class="fa fa-caret-left"></span>
+                              <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                              <span class="fa fa-caret-right"></span>
+                              <span class="sr-only">Next</span>
+                        </a>
 
                         <!-- Wrapper for slides -->
                         <div class="carousel-inner" style="background:#15161D;">
                               
                               <div class="item active">
                                     <img src="/images/loan-banner.jpg" alt="Loan">
-                                    <div class="text-left">
+                                    <div class="text-right">
                                           <p></p>
                                           @auth
                                           @if(Auth::user()->role_name == 'cooperative')
@@ -86,15 +95,7 @@
                               </div>
                         </div>
 
-                        <!-- Left and right controls -->
-                        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                              <span class="fa fa-caret-left"></span>
-                              <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                              <span class="fa fa-caret-right"></span>
-                              <span class="sr-only">Next</span>
-                        </a>
+                      
                   </div>
 
 
