2bb3e66e640f1a406c2bfa785e61023a141981f2
Esther Eze <estherakowe@yahoo.com>
2024-10-28 17:59:07 +0100

    Update member-order.blade.php

--- resources/views/cooperative/member-order.blade.php
+++ resources/views/cooperative/member-order.blade.php
@@ -403,10 +403,28 @@
                         </div>
                   </div>
 
-                  <span id="monthError" style="display:none;"><a href="{{ url('account-settings') }}" class="btn btn-ghost-danger active ms-auto">Click here
-                  to set your loan duration and interest rate</a></span>
 
-              
+                  <div class="alert alert-danger alert-important alert-dismissible" role="alert" id="monthError" style="display:none;">
+                        <div class="d-flex">
+                              <div>
+                                    <!-- Download SVG icon from http://tabler-icons.io/i/alert-circle -->
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon" width="24"
+                                          height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                          fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                          <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+                                          <path d="M12 8v4" />
+                                          <path d="M12 16h.01" />
+                                    </svg>
+
+
+                              </div>
+                              <div><a href="{{ url('account-settings') }}"
+                                          class="text-white">Click here
+                                          to set your loan duration and interest rate</a></div>
+                        </div>
+                        <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+                  </div>
 
                   <!-- Alert stop --->
                   <strong class="text-danger">Any order that is approve can not be "cancel"</strong>
@@ -539,7 +557,8 @@
                                                                               class="form-control" disabled>
                                                                         <input type="hidden" id="loanTenure"
                                                                               value="{{$getAdminLoanDuration}}">
-                                                                              <input type="hidden" id="ratetype" value="{{$loanTypeID}}">
+                                                                        <input type="hidden" id="ratetype"
+                                                                              value="{{$loanTypeID}}">
 
                                                                         <!-- <a class="dropdown-item"
                                                                               href="request-product-loan/{{ $order->id }}">
@@ -788,7 +807,7 @@ function cal_interest() {
       let duration = document.getElementById('loanTenure').value;
       let order = document.getElementById('order_id').value;
 
-    
+
 
       if (duration == null || duration == "" || duration == 0) {
             document.getElementById('monthError').style.display = 'block';
