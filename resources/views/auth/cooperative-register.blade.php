c21abd980466b4e6661c32b13541d62d495d5e08
Esther Eze <estherakowe@yahoo.com>
2024-11-28 06:23:19 +0100

    Update cooperative-register.blade.php

--- resources/views/auth/cooperative-register.blade.php
+++ resources/views/auth/cooperative-register.blade.php
@@ -3,23 +3,19 @@
 
 <head>
       <meta charset="utf-8">
-
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
       <link rel="stylesheet" type="text/css" href="{{ asset('admin/css/adminx.css') }}" media="screen" />
-
       <!-- CSRF Token -->
       <meta name="csrf-token" content="{{ csrf_token() }}">
-
       <title>{{ config('app.name', 'Lascocomart') }}</title>
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-
       <script async src="https://www.google.com/recaptcha/api.js"></script>
       <!-- Scripts -->
       <script src="{{ asset('js/app.js') }}" defer></script>
 
       <!-- Fonts -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
       <link rel="dns-prefetch" href="//fonts.gstatic.com">
       <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
@@ -172,7 +168,7 @@
                                                       <div class="col-md-6 ">
                                                             <span class="small text-primary">minimum length: 6</span>
                                                             <div class="form-group">
-                                                            <div class="input-group input-group-flat">
+                                                            <div class="input-group input-group-flat"  id="show_hide_password">
                                                                   <input id="password" type="password"
                                                                         class="@error('password') is-invalid @enderror input-field form-control "
                                                                         name="password" required
@@ -202,7 +198,7 @@
 
                                                       <div class="col-md-6 ">
                                                             <div class="form-group">
-                                                                   <div class="input-group input-group-flat">
+                                                                   <div class="input-group input-group-flat" >
                                                                   <input id="password-confirm" type="password"
                                                                         class="form-control"
                                                                         name="password_confirmation" required
