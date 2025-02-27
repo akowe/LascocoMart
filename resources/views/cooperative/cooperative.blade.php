c23f94f2fb20abb9d7739b5615a36f0a5b1ab20f
Esther Eze <estherakowe@yahoo.com>
2025-02-27 06:16:06 +0100

    Update cooperative.blade.php

--- resources/views/cooperative/cooperative.blade.php
+++ resources/views/cooperative/cooperative.blade.php
@@ -11,21 +11,22 @@
                               Dashboard
                         </div>
                         <h2 class="page-title">
-                              <span>Cooperative ID:&nbsp;</span> {{Auth::user()->code}}
-
-                              <a href="" alt="Copy" title="Copy" class="text-danger"
-                                    onclick="copyToClipboard('{{Auth::user()->code}}')">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-copy  "
-                                          width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5"
-                                          stroke="currentColor" fill="none" stroke-linecap="round"
-                                          stroke-linejoin="round">
-                                          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                          <path
-                                                d="M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" />
-                                          <path
-                                                d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1" />
-                                    </svg></a>
+                              <span>Cooperative:&nbsp;</span>
                         </h2>
+                        <strong class="text-info"> Referral link:</strong> <small><a
+                                    href="{{$shareUrl}}">{{$shareUrl}}</a></small>
+
+                        <a href="" alt="Copy" title="Copy" class="text-danger"
+                              onclick="copyToClipboard('{{$shareUrl}}')">
+                              <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-copy  "
+                                    width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                    fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path
+                                          d="M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" />
+                                    <path
+                                          d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1" />
+                              </svg></a>
                   </div>
                   <p></p>
             </div>
