d82ca7826a950bd8038b344a423d354e82fe810f
Esther Eze <estherakowe@yahoo.com>
2025-02-27 06:19:06 +0100

    Update cooperative-list.blade.php

--- resources/views/company/cooperative-list.blade.php
+++ resources/views/company/cooperative-list.blade.php
@@ -310,24 +310,30 @@
                                                       @endif
 
                                                       <div class="text-secondary">{{ $details->email }}</div>
-                                                      <strong>link</strong>: <a href="" alt="Copy" title="Copy"
-                                                            class="text-dark"
-                                                            onclick="copyToClipboard('{{route('register-member', ['user' => $details->code, 'reference' => '2/' ])}}')">
-                                                            <svg xmlns="http://www.w3.org/2000/svg"
-                                                                  class="icon icon-tabler icon-tabler-copy  " width="24"
-                                                                  height="24" viewBox="0 0 24 24" stroke-width="1.5"
-                                                                  stroke="currentColor" fill="none"
-                                                                  stroke-linecap="round" stroke-linejoin="round">
-                                                                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                                  <path
-                                                                        d="M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" />
-                                                                  <path
-                                                                        d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1" />
-                                                            </svg>
-                                                            Copy
-                                                      </a>
-                                                      
-
+                                                      <p></p>
+                                                      <div> 
+                                                            <strong>link</strong>: <a
+                                                                  href="{{route('register-member', ['user' => $details->code, 'reference' => '2/' ])}}">
+                                                                  {{route('register-member', ['user' => $details->code, 'reference' => '2/' ])}}
+                                                            </a>
+                                                            <a href="" alt="Copy" title="Copy" class="text-dark"
+                                                                  onclick="copyToClipboard('{{route('register-member', ['user' => $details->code, 'reference' => '2/' ])}}')">
+                                                                  <svg xmlns="http://www.w3.org/2000/svg"
+                                                                        class="icon icon-tabler icon-tabler-copy  "
+                                                                        width="24" height="24" viewBox="0 0 24 24"
+                                                                        stroke-width="1.5" stroke="currentColor"
+                                                                        fill="none" stroke-linecap="round"
+                                                                        stroke-linejoin="round">
+                                                                        <path stroke="none" d="M0 0h24v24H0z"
+                                                                              fill="none" />
+                                                                        <path
+                                                                              d="M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" />
+                                                                        <path
+                                                                              d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1" />
+                                                                  </svg>
+                                                                  Copy
+                                                            </a>
+                                                      </div>
 
 
                                                 </div>
